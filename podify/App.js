11942dc94b3fed52a96841062630f821ec8dc683
Weldon Fisher <chamuka15d@gmail.com>
2025-04-16 18:22:18 +0530

    Changes

--- podify/App.js
+++ podify/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={require("./assets/images/Logo.png")} style={styles.logo} />
+      <Text>Welcome to Podify!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0D0D12",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 160,
+    height: 45,
+    objectFit: "contain",
   },
 });
